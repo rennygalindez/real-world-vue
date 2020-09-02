6f97e69fc76cce096111268ad78a6c3d7c7e9b15
Renny Galindez <rfgalindez@gmail.com>
2020-09-02 10:34:42 -0500

    create and use some new named routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,31 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
-Vue.use(VueRouter);
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventList from '@/views/EventList'
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'Home',
+    component: EventList
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: '/event',
+    name: 'event-show',
+    component: () => import(/* webpackChunkName: "show" */ '@/views/EventShow')
+  },
+  {
+    path: '/event-create',
+    name: 'event-create',
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "create" */ '@/views/EventCreate')
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
