0c6d081cb34d79e270f6bddfbc55711f3eb9e9ed
Renny Galindez <rfgalindez@gmail.com>
2020-09-02 22:51:09 -0500

    Create NotFound component to notfound routes

---
+++ src/views/RouteNotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Route not Found 404</h1>
+    <BaseIcon name="sun" height="48" width="48" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
