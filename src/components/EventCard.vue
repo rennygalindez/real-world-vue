49f2bce8a01d6f255a8c634fbc6c9276ba854fb2
Renny Galindez <rfgalindez@gmail.com>
2020-09-03 19:00:03 -0500

    Update EventCard to recive a prop with the an event

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,31 +1,26 @@
 <template>
-  <router-link
-    class="event-link"
-    :to="{ name: 'event-show', params: { id: '1' } }"
-  >
-    <div class="event-card -shadow">
-      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
-      <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users" />
-      <span>{{ event.attendees.length }} attending</span>
-    </div>
-  </router-link>
+  <div>
+    <router-link
+      class="event-link"
+      :to="{ name: 'event-show', params: { id: event.id } }"
+    >
+      <div class="event-card -shadow">
+        <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+        <h4 class="title">{{ event.title }}</h4>
+        <BaseIcon name="users">
+          {{ event.attendees.length }} attending</BaseIcon
+        >
+      </div>
+    </router-link>
+  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: 'Beach Cleanup',
-        date: 'Tue May 15 2018',
-        time: '6:00',
-        attendees: [
-          { id: 'abc123', name: 'Adam Jahr' },
-          { id: 'def456', name: 'Gregg Pollack' }
-        ]
-      }
+  props: {
+    event: {
+      type: Object,
+      require: true
     }
   }
 }
