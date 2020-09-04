b9b1023540d853594b6b1adc55e40bcbead65d4b
Renny Galindez <rfgalindez@gmail.com>
2020-09-03 19:06:00 -0500

    Add link to Home

--- src/views/RouteNotFound.vue
+++ src/views/RouteNotFound.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Route not Found 404</h1>
-    <BaseIcon name="sun" height="48" width="48" />
+    <router-link :to="{ name: 'event-list ' }">Home</router-link>
   </div>
 </template>
 
