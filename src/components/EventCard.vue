1d911e45e07625892723e10875325d0ccda262bb
Renny Galindez <rfgalindez@gmail.com>
2020-09-05 00:12:07 -0500

    Fix event attendes issue

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -8,8 +8,9 @@
         <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
         <h4 class="title">{{ event.title }}</h4>
         <BaseIcon name="users">
-          {{ event.attendees.length }} attending</BaseIcon
-        >
+          {{ event.attendees ? event.attendees.length : 0 }}
+          attending
+        </BaseIcon>
       </div>
     </router-link>
   </div>
