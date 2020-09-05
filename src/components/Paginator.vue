868cdfa355c42f7440a74b89b9f95ce4fc98697f
Renny Galindez <rfgalindez@gmail.com>
2020-09-05 00:13:01 -0500

    Created pagination component

---
+++ src/components/Paginator.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      <template v-if="page != 1">
+        <router-link
+          :to="{ name: 'event-list', query: { page: page - 1 } }"
+          rel="prev"
+          >Prev Page</router-link
+        >|
+      </template>
+      <template v-if="lastLink">
+        <router-link :to="{ name: 'event-list', query: { page: page + 1 } }"
+          >Next Page</router-link
+        >
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  props: { page: { type: [Number] } },
+  computed: {
+    ...mapState({ eventsTotal: 'eventsTotal' }),
+    lastLink() {
+      return this.eventsTotal > this.page * 3
+    }
+  }
+}
+</script>
+
+<style scoped></style>
