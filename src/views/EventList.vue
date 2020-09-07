0eead989c6273d59581892dd313ac8eb93f95341
Renny Galindez <rfgalindez@gmail.com>
2020-09-07 15:27:47 -0500

    Change EventList view to use vuex modules

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,25 +1,36 @@
 <template>
   <div>
     <h1>Event list</h1>
-    <EventCard v-for="(event, index) of events" :key="index" :event="event" />
+    <template v-if="loading">
+      <img :src="require('../../public/Pulse-1s-200px.gif')" />
+    </template>
+    <template v-else>
+      <EventCard v-for="(event, index) of events" :key="index" :event="event" />
+      <paginator :page="page" />
+    </template>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventsService from '@/services/EventsService'
-
+import Paginator from '@/components/Paginator.vue'
+import { mapActions, mapState } from 'vuex'
 export default {
-  components: { EventCard },
+  components: { EventCard, Paginator },
   data() {
-    return {
-      events: []
+    return { loading: true }
+  },
+  computed: {
+    ...mapState('events', { events: 'events' }),
+    page() {
+      return parseInt(this.$route.query.page) || 1
     }
   },
+  methods: { ...mapActions('events', { fetchEvents: 'fetchEvents' }) },
   created() {
-    EventsService.getEvents()
-      .then(({ data }) => (this.events = data))
-      .catch(console.log)
+    this.fetchEvents({ limit: 3, page: this.page }).then(
+      () => (this.loading = false)
+    )
   }
 }
 </script>
