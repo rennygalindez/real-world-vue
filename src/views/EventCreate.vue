63dcc75d9076069b1cc0b874594bf58a54a1ab38
Renny Galindez <rfgalindez@gmail.com>
2020-09-07 15:26:49 -0500

    Crete EventCreate view

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,11 +1,102 @@
 <template>
-  <div>
-    <h1>Event Create View</h1>
-  </div>
+  <form @submit.prevent="createEvent">
+    <label>Select a category</label>
+    <select v-model="event.category">
+      <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+    </select>
+    <h3>Name & describe your event</h3>
+    <div class="field">
+      <label>Title</label>
+      <input
+        v-model="event.title"
+        type="text"
+        placeholder="Add an event title"
+        required
+      />
+    </div>
+    <div class="field">
+      <label>Description</label>
+      <input
+        v-model="event.description"
+        type="text"
+        placeholder="Add a description"
+      />
+    </div>
+    <h3>Where is your event?</h3>
+    <div class="field">
+      <label>Location</label>
+      <input
+        v-model="event.location"
+        type="text"
+        placeholder="Add a location"
+        required
+      />
+    </div>
+    <h3>When is your event?</h3>
+    <div class="field">
+      <label>Date</label>
+      <datepicker v-model="event.date" placeholder="Select a date" />
+    </div>
+    <div class="field">
+      <label>Select a time</label>
+      <select v-model="event.time">
+        <option v-for="time in times" :key="time">{{ time }}</option>
+      </select>
+    </div>
+    <input type="submit" class="button -fill-gradient" value="Submit" />
+  </form>
 </template>
 
 <script>
-export default {}
+import { mapActions, /* mapGetters, */ mapState } from 'vuex'
+import datepicker from 'vuejs-datepicker'
+export default {
+  data() {
+    let times = []
+    for (let i = 0; i < 24; i++) {
+      times.push(`${i}:00`)
+    }
+    return { times, event: this.createFreshEvent() }
+  },
+  computed: {
+    ...mapState([/* 'user', */ 'categories']),
+    ...mapState('users', {
+      user: 'user'
+    })
+  },
+  methods: {
+    ...mapActions('events', { addEvent: 'createEvent' }),
+    createEvent() {
+      this.addEvent(this.event)
+        .then(() => {
+          this.$router.push({
+            name: 'event-show',
+            params: { id: this.event.id }
+          })
+          this.event = this.createFreshEvent()
+        })
+        .catch(() => {
+          console.log('There was a problem creating your event.')
+        })
+    },
+    createFreshEvent() {
+      let user = this.user
+      let id = Math.floor(Math.random() * 10000)
+      return {
+        id: id,
+        category: '',
+        organizer: user,
+        title: '',
+        description: '',
+        location: '',
+        date: '',
+        time: '',
+        attendees: []
+      }
+    }
+  },
+  components: { datepicker }
+}
 </script>
 
 <style scoped></style>
