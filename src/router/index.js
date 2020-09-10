3f255369fe8061d34261f1d5f4bc66aa3a1f80b4
Renny Galindez <rfgalindez@gmail.com>
2020-09-09 23:04:25 -0500

    Modife and create new routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import EventList from '@/views/EventList'
+import NProgress from 'nprogress'
+import store from '@/store'
 Vue.use(VueRouter)
 
 const routes = [
@@ -19,11 +21,32 @@ const routes = [
     path: '/event/:id',
     name: 'event-show',
     props: true,
+    beforeEnter: (to, from, next) => {
+      store
+        .dispatch('events/fetchEvent', to.params.id)
+        .then(event => {
+          to.params.event = event
+          next()
+        })
+        .catch(err => {
+          if (err.response && err.response.status === 404) {
+            next({ name: 'notFound', params: { resource: 'event' } })
+          } else next({ name: 'network-issue', params: { err: err.message } })
+        })
+    },
     component: () => import(/* webpackChunkName: "show" */ '@/views/EventShow')
   },
   {
-    path: '*',
-    name: 'jocker',
+    path: '/networissue',
+    name: 'network-issue',
+    component: () =>
+      import(/* webpackChunkName: "notFound" */ '@/views/NetworkIssue')
+  },
+  {
+    path: '/notFound404',
+    name: 'notFound',
+    alias: '*',
+    props: true,
     component: () =>
       import(/* webpackChunkName: "notFound" */ '@/views/RouteNotFound')
   }
@@ -34,5 +57,11 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
-
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+router.afterEach(() => {
+  NProgress.done()
+})
 export default router
