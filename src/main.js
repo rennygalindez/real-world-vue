1969e7e1f8f68c80240289eb006198e132bd1c5e
Renny Galindez <rfgalindez@gmail.com>
2020-09-09 23:04:43 -0500

    add nprogress

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/components/globalComponents'
+import 'nprogress/nprogress.css'
 
 Vue.config.productionTip = false
 
