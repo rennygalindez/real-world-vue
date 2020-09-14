043495ef29b4c8b34dcb35943434358807d76e0f
Renny Galindez <rfgalindez@gmail.com>
2020-09-14 16:31:03 -0500

    BaseInput component, to be reusable in diferents forms

---
+++ src/components/globalComponents/BaseInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input :value="value | upperCap" v-bind="$attrs" v-on="listeners" />
+  </div>
+</template>
+
+<script>
+import formFieldMixin from '@/mixins/formFieldMixin'
+export default {
+  mixins: [formFieldMixin],
+  computed: {
+    listeners() {
+      return { ...this.$listeners, input: this.sendValue }
+    }
+  },
+  filters: {
+    upperCap(x) {
+      return x.toUpperCase()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
