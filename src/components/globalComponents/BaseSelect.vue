844a84643c168e4526dcba7e13f10b81131042d4
Renny Galindez <rfgalindez@gmail.com>
2020-09-14 16:37:20 -0500

    BaseSelectcompoentn definition, to be reusable

---
+++ src/components/globalComponents/BaseSelect.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <label v-if="label" v-bind="$attrs">{{ label }}</label>
+    <select :value="value" v-on="$listeners" @change="sendValue">
+      <option
+        :value="option"
+        v-for="option of options"
+        :key="option"
+        selected="option===value"
+        >{{ option }}</option
+      >
+    </select>
+  </div>
+</template>
+
+<script>
+import formFieldMixin from '@/mixins/formFieldMixin'
+
+export default {
+  mixins: [formFieldMixin],
+  props: {
+    options: { type: Array, require: true }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
