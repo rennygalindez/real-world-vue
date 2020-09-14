e4d738b3b718ed0fa5d99660908c269239b08354
Renny Galindez <rfgalindez@gmail.com>
2020-09-14 17:20:19 -0500

    Created BaseError compoenent

---
+++ src/components/globalComponents/BaseErrorItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="fieldError">
+    <p v-if="!fieldRequired" class="errorMessage">{{ errorMessage }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    fieldError: { type: Boolean, require: true },
+    fieldRequired: { type: Boolean, require: true },
+    errorMessage: { type: String, require: true }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
