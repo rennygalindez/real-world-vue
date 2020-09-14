d7386d3c7bdbfe8012b4ecf435d12492b5c7dcee
Renny Galindez <rfgalindez@gmail.com>
2020-09-14 16:35:22 -0500

    A view to be render when network issues happens

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Network Issue</h1>
+    <h3>
+      It looks like there was a network issue, please press back button and try
+      again.
+    </h3>
+    <router-link :to="{ name: 'event-list' }"> go to Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
