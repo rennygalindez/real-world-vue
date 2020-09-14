6de8ed715ebb7a49abca958e6d496f7f485e7e96
Renny Galindez <rfgalindez@gmail.com>
2020-09-14 16:29:20 -0500

    Vuelidate instalation and config

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/components/globalComponents'
+import Vuelidate from 'vuelidate'
 import 'nprogress/nprogress.css'
+Vue.use(Vuelidate)
+/* Vue.mixin({
+  created() {
+    console.log('Hello, Im global!')
+  }
+}) */
 
 Vue.config.productionTip = false
 
